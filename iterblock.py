ba17cd860bb1febf36c84286ebb0ac39e2f3d444
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-05-15 22:42:17 -0600

    Update iterblock.py

--- iterblock.py
+++ iterblock.py
@@ -1,13 +1,38 @@
-import sys
 import itertools
+import time
+import sys
+import os
+
+from datetime import datetime
 
 characters = (str(sys.argv[1]))
 counts = (int(sys.argv[2]))
 
 iblock = itertools.product(characters, repeat=counts)
 
+def timeslice():
+    '''Make a global TIMESTAMP with datetime module.'''
+    global TIMESTAMP
+    TIMESTAMP = datetime.now()
+    return(TIMESTAMP)
+
+def diskcheck():
+    '''Check disk usage'''
+    timeslice()
+    sized = 'df . | tail -n1 | awk \'{print $5}\' | grep "^9\|^100"'
+    alertsize = os.system(sized)
+    timeslice()
+    if alertsize == 0:
+        timeslice()
+        sys.exit("Exiting because disk space is 90% or greater in the $pwd")
+    else:
+        timeslice()
+    timeslice()
+
 def iterated(iblock):
     for i in iblock:
+        diskcheck()
         print (''.join(i))
 
-iterated(iblock)        
+
+iterated(iblock)
